73d2cfeafbd2551484fcaeff0e680552e1e6cac0
th005151 <rg005151@gmail.com>
2026-04-04 15:26:31 +0800

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // works for both username.github.io and username.github.io/repo
+  base: '/RTL-viewer/',
 })
